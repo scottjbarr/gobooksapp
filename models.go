0008bbccb17133d2d2d7399a18483227d30010e4
Scott Barr <scottjbarr@gmail.com>
2015-07-07 19:45:15 +1000

    Serialise model attributes as lower case

--- models.go
+++ models.go
@@ -2,8 +2,8 @@ package main
 
 // Book struct for the books table
 type Book struct {
-	ISBN   string  `gorm:"column:isbn;primary_key"`
-	Title  string  `gorm:"column:title"`
-	Author string  `gorm:"column:author"`
-	Price  float32 `gorm:"column:price"`
+	ISBN   string  `gorm:"column:isbn;primary_key" json:"isbn"`
+	Title  string  `gorm:"column:title" json:"title"`
+	Author string  `gorm:"column:author" json:"author"`
+	Price  float32 `gorm:"column:price" json:"price"`
 }
